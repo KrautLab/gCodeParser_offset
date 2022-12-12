5eb95c7e8016c753114e44a904c68d6aeeffc74a
KrautLab <118902239+KrautLab@users.noreply.github.com>
2022-12-12 16:29:54 +0100

    output fertig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from gcodeparser import GcodeParser
-
+nozzle_size=0.4
 # open gcode file and store contents as variable
 with open(r'C:\Users\alorenz\PycharmProjects\gCodeParser\100prozentcube.gcode', 'r') as f:
   gcode = f.read()
@@ -17,5 +17,10 @@ for line in parsed_gcode.lines:
   if switch==1:
     offset = not offset
 
-  print(offset)
+  if line.params.get('X') != None:
+    x_coord=float(line.get_param('X'))
+    if offset == True:
+      line.update_param('X', x_coord + nozzle_size * 0.5)
+
+  print(line.gcode_str)
 
